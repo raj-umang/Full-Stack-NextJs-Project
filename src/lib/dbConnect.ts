bbfe44bb1d6868c54445d2389d29356004c91c2c
raj-umang <jackerumang@gmail.com>
2024-05-25 00:42:55 +0530

    done with the sign up backend part

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log('Already connected to database');
+        return        
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+
+        connection.isConnected = db.connections[0].readyState
+        console.log(db.connections);
+        
+        console.log("DB Connected Successfully");
+
+    } catch (error) {
+        
+        console.log('DB Connect Failed', error);
+        
+        process.exit(1)
+    }
+
+}
+
+export default dbConnect;
