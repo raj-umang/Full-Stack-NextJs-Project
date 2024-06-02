3bc08dc5dc2efc93029d9b9bdde72cd9289d2166
raj-umang <jackerumang@gmail.com>
2024-06-02 19:06:29 +0530

    verify code done

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 
 const page = () => {
-  return (
-    <div>Sign in page</div>
-  )
-}
+  return <div>Sign in page</div>;
+};
 
-export default page
+export default page;
