eb96a07ac5c4dccfb666095ce30028657cb05ab8
raj-umang <jackerumang@gmail.com>
2024-07-05 09:34:38 +0530

    fixed error in sign in

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -25,7 +25,7 @@ import { Loader2 } from "lucide-react";
 import { signInSchema } from "@/schemas/signInSchema";
 import { signIn } from "next-auth/react";
 
-const page = () => {
+export default function SignUpForm() {
   const { toast } = useToast();
   const router = useRouter();
 
@@ -108,5 +108,4 @@ const page = () => {
       </div>
     </div>
   );
-};
-export default page;
+}
